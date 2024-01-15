8bba28f0d160be1db284a93bca03216e7b93dd96
aymen hadouara <aymenhadouara@gmail.com>
2024-01-16 00:24:02 +0100

    add db

--- api/users.js
+++ api/users.js
@@ -1,4 +1,7 @@
-module.exports = (req, res) => {
+import { MongoClient } from "mongodb";
+import { config } from "dotenv";
+
+module.exports = async (req, res) => {
   if (req.method == "GET") {
     res.json([
       {
@@ -15,8 +18,35 @@ module.exports = (req, res) => {
       },
     ]);
   } else if (req.method == "POST") {
-    res.status(200).json({
-      message: "Success post request !!",
-    });
+    let mongoClient;
+    try {
+      mongoClient = new MongoClient(process.env.DB_URI);
+      console.log("Connecting to MongoDB Atlas cluster...");
+      await mongoClient.connect();
+      console.log("Successfully connected to MongoDB Atlas!");
+
+      // go to serverless db
+      const db = mongoClient.db("serverless");
+      // go to users collection
+      const collection = db.collection("users");
+
+      //create user document
+      const userDocument = {
+        name: req.body.fullname,
+        birthdate: new Date(2000, 12, 27),
+        address: { street: "Aflou 03001", city: "Aflou", state: "AF" },
+      };
+
+      await collection.insertOne(userDocument);
+
+      res.status(204).json({
+        message: "done",
+      });
+    } catch (error) {
+      res.status(404);
+      console.error("Connection to MongoDB Atlas failed!", error);
+    } finally {
+      await mongoClient.close();
+    }
   }
 };
